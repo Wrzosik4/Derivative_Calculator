26c22444586b024a3591d58f79f6961dfb632f55
Denny <145674354+Wrzosik4@users.noreply.github.com>
2024-09-15 10:56:15 +0200

    Update 0.2 m_action.py

--- m_action.py
+++ m_action.py
@@ -1,50 +1,25 @@
 import sympy as sp
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from PIL import Image, ImageTk
-import tkinter as tk
-import io
-from sympy.parsing.sympy_parser import parse_expr
 
-def correctExpression(expression):
-    expression = expression.replace(' ', '')
-    corrected = ''
-    length = len(expression)
+def calculableDerivatives(fun):
+    var = sp.symbols('x')
+    fun = sp.sympify(fun)
+    derivatives = sp.diff(fun,var)
+    derivatives.simplify()
 
-    for i in range(length):
-        corrected += expression[i]
-        if (i < length - 1) and (
-                (expression[i].isnumeric() or expression[i] in ')') and
-                (expression[i + 1].isalpha() or expression[i + 1] == '(')
-        ):
-            corrected += '*'
+    return derivatives
 
-    corrected = corrected.replace('exp(', 'exp(')
-    return corrected
+def calculatingTheIndefiniteIntegral(fun):
+    var = sp.symbols('x')
+    fun = sp.sympify(fun)
+    integral = sp.integrate(fun,var)
+    integral.simplify()
 
+    return integral
 
-def readLatex(latex_str):
-    fig, ax = plt.subplots(figsize=(5, 2))
-    ax.text(0.5, 0.5, f'${latex_str}$', fontsize=20, ha='center', va='center')
-    ax.axis('off')
+def definiteIntegralCalculator(fun, a, b):
+    var = sp.symbols('x')
+    fun = sp.sympify(fun)
+    integral = sp.integrate(fun, (var, a, b))
+    integral.simplify()
 
-    canvas = FigureCanvas(fig)
-    buf = io.BytesIO()
-    canvas.print_png(buf)
-    plt.close(fig)
-
-    buf.seek(0)
-    image = Image.open(buf)
-    return ImageTk.PhotoImage(image)
-
-
-def oblicz_pochodne(fun, var):
-    try:
-        fun = correctExpression(fun)
-        x = sp.symbols(var)
-        fun = parse_expr(fun, evaluate=False)
-        derivative = sp.diff(fun, x)
-        simplified_derivative = sp.simplify(derivative)
-        return simplified_derivative
-    except Exception as e:
-        return f'Wystąpił błąd: {e}'
+    return integral
