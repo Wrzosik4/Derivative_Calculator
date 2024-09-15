81c69e106e28e1e652212f52210f066662c9d7c0
Denny <145674354+Wrzosik4@users.noreply.github.com>
2024-09-15 10:56:44 +0200

    Update 0.1 main.py

--- main.py
+++ main.py
@@ -1,40 +1,92 @@
 from m_action import *
-import sympy as sp
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from PIL import Image, ImageTk
 import tkinter as tk
-import io
+from tkinter import ttk
 
-def calculatorDerivative():
-    function = function_entry.get()
-    variable = 'x'
+def displayResult(result):
+    result_label.config(text=f'Wynik: {result}')
 
-    function = correctExpression(function)
-    result = oblicz_pochodne(function, variable)
+def downloadDerivative():
+    function = fun_entry_derivatives.get()
+    try:
+        result = calculableDerivatives(function)
+        displayResult(result)
+    except Exception as e:
+        displayResult(f'Błąd: {e}')
 
-    print("Derivative result:", result)
+def downloadIndefiniteIntegral():
+    function = fun_entry_integrals.get()
+    try:
+        result = calculatingTheIndefiniteIntegral(function)
+        displayResult(result)
+    except Exception as e:
+        displayResult(f'Błąd: {e}')
 
-    latex_result = sp.latex(result)
-    result_image = readLatex(latex_result)
-
-    result_label.config(image=result_image)
-    result_label.image = result_image
-
+def downloadDefiniteIntegral():
+    function = fun_entry_definite_integrals.get()
+    try:
+        a = float(a_entry.get())
+        b = float(b_entry.get())
+        result = definiteIntegralCalculator(function, a, b)
+        displayResult(result)
+    except ValueError:
+        displayResult('Błąd: Proszę wprowadzić poprawne liczby dla a i b.')
+    except Exception as e:
+        displayResult(f'Błąd: {e}')
 
 root = tk.Tk()
-root.title("Kalkulator Pochodnych")
+root.title('Kalkulator')
+root.geometry('600x600')
 
-tk.Label(root, text="Podaj funkcję:").pack()
-function_entry = tk.Entry(root)
-function_entry.pack(fill=tk.X, padx=10)
+notebook = ttk.Notebook(root)
+notebook.pack(expand=1, fill='both')
+
+# Frames for each tab
+derivative_frame = ttk.Frame(notebook)
+integral_frame = ttk.Frame(notebook)
+definite_integral_frame = ttk.Frame(notebook)
+
+# Add frames to notebook
+notebook.add(derivative_frame, text='Pochodne')
+notebook.add(integral_frame, text='Całki Nieoznaczone')
+notebook.add(definite_integral_frame, text='Całki Oznaczone')
+
+# Pochodne
+tk.Label(derivative_frame, text='Podaj funkcje:').pack()
+fun_entry_derivatives = tk.Entry(derivative_frame)
+fun_entry_derivatives.pack()
+
+derivatives_button = tk.Button(derivative_frame, text='Pochodna', command=downloadDerivative)
+derivatives_button.pack()
 
 
-calculate_button = tk.Button(root, text="Oblicz Pochodną", command=calculatorDerivative)
-calculate_button.pack(pady=10)
 
-# Etykieta do wyświetlania wyniku
-result_label = tk.Label(root)
-result_label.pack(pady=10)
+# Całki nieoznaczone
+tk.Label(integral_frame, text='Podaj funkcje:').pack()
+fun_entry_integrals = tk.Entry(integral_frame)
+fun_entry_integrals.pack()
+
+indefinite_integral_button = tk.Button(integral_frame, text='Całka nieoznaczona', command=downloadIndefiniteIntegral)
+indefinite_integral_button.pack()
+
+tk.Label(definite_integral_frame, text='Podaj a:').pack()
+a_entry = tk.Entry(definite_integral_frame)
+a_entry.pack()
+
+tk.Label(definite_integral_frame, text='Podaj b:').pack()
+b_entry = tk.Entry(definite_integral_frame)
+b_entry.pack()
+
+
+
+# Całki oznaczone
+tk.Label(definite_integral_frame, text='Podaj funkcje:').pack()
+fun_entry_definite_integrals = tk.Entry(definite_integral_frame)
+fun_entry_definite_integrals.pack()
+
+definite_integral_button = tk.Button(definite_integral_frame, text='Całka oznaczona', command=downloadDefiniteIntegral)
+definite_integral_button.pack()
+
+result_label = tk.Label(root, text='Wynik:', font = ('Helvetica', 16), anchor = 'center')
+result_label.pack(expand=True, fill='both')
 
 root.mainloop()
