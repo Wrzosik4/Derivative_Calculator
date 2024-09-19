995d5180ad1286d6bcb37105980cec510f0d2374
Denny <145674354+Wrzosik4@users.noreply.github.com>
2024-09-19 08:20:18 +0200

    Update m_action.py
    I implemented a function that reduces mathematical expressions into simpler forms.

--- m_action.py
+++ m_action.py
@@ -1,25 +1,42 @@
 import sympy as sp
 
+def simplifyFun(fun):
+    try:
+        simplified = sp.trigsimp(fun)
+        simplified = sp.simplify(simplified)
+        simplified = sp.expand(simplified)
+        simplified = sp.factor(simplified)
+        simplified = sp.collate(simplified, sp.symbols('x'))
+        simplified = sp.logcombine(simplified, force=True)
+
+        return simplified
+    except Exception as e:
+        print(f'Error: {e}')
+        return fun
+
+
 def calculableDerivatives(fun):
     var = sp.symbols('x')
     fun = sp.sympify(fun)
     derivatives = sp.diff(fun,var)
-    derivatives.simplify()
+    simplified_derivatives = simplifyFun(derivatives)
 
-    return derivatives
+    return sp.latex(simplified_derivatives)
+
 
 def calculatingTheIndefiniteIntegral(fun):
     var = sp.symbols('x')
     fun = sp.sympify(fun)
     integral = sp.integrate(fun,var)
-    integral.simplify()
+    simplified_integral = simplifyFun(integral)
 
-    return integral
+    return sp.latex(simplified_integral) + ' + C'
+
 
 def definiteIntegralCalculator(fun, a, b):
     var = sp.symbols('x')
     fun = sp.sympify(fun)
     integral = sp.integrate(fun, (var, a, b))
-    integral.simplify()
+    simplified_integral = simplifyFun(integral)
 
-    return integral
+    return sp.latex(simplified_integral) + ' + C'
